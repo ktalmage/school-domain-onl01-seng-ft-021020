1ff1c2290181ec0b929bb5eb66d747979d450404
Kyle Talmage <ktalmage85@gmail.com>
2020-02-20 23:49:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student,grade)
+   
+    roster[level] ||= []
+    roster[level] << student
+  end 
+  
+  def grade(grade)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+      end 
+    end 
+  end
+end
+    
